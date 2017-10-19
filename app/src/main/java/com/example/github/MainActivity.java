bd8effa2486ec3119a396103d844809566597533
Freddy Maila <fredosw@hotmail.com>
2017-10-19 09:52:50 -0500

    he puesto un comentario mainactivity.java

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.github;
 
+//ejemplo master android
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
